c07dee5e4d868def293dd4a6c9494169b1234d65
Anton <sokolplus3783@gmail.com>
2023-03-24 17:26:22 +0200

    create tests and matcher

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,15 @@
+CREATE TABLE equation{
+  equation_id SERIAL PRIMARY key,
+  record VARCHAR (250)
+}
+
+CREATE TABLE root {
+    root_id SERIAL PRIMARY key,
+    root double precision
+}
+
+CREATE TABLE equation_root (
+  equation_id INTEGER REFERENCES equation(equation_id),
+  root_id INTEGER REFERENCES root(root_id),
+  PRIMARY KEY (equation_id, root_id)
+);
---
+++ src/test/java/org/example/EquationMatcherTest.java
@@ -0,0 +1,41 @@
+package org.example;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+class EquationMatcherTest {
+
+  @DisplayName("Equation contain valid char")
+  @ParameterizedTest(name = "{index} => expected={1}, input={0}")
+  @CsvSource({
+      "2*x+5ad=17,false",
+      "2*x+5=17, true",
+      "-1.3*5cc/x=1.2, false",
+      "5-(2x+1)=15, true",
+      "(5x -1)/(2-2x)=10, true"
+  })
+  public void testCorrectSymbols(String input, boolean expected) {
+    EquationMatcher matcher = EquationMatcher.getEquationMatcherForChar();
+    boolean result = matcher.validateChar(input);
+    assertEquals(expected, result);
+  }
+
+
+  @DisplayName("Equation brackets closed correct")
+  @ParameterizedTest(name = "{index} => expected={1}, input={0}")
+  @CsvSource({
+      "(2*x)+5=17,true",
+      "(2*x+)5=17, false",
+      "((-1.3*5)/x)+3=1.2, false",
+      "(5-(2x+1)=15, true",
+      "(5x -1)/(2-2x)=10, true"
+  })
+  public void testCorrectBrackets(String input, boolean expected) {
+    EquationMatcher matcher = EquationMatcher.getEquationMatcherForRoundBrackets();
+    boolean result = matcher.validateChar(input);
+    assertEquals(expected, result);
+  }
+}
